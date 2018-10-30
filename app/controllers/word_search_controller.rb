e38721f3a51d040144885d0bf13b00f43e695540
mmbensalah <mmbensalah@gmail.com>
2018-10-30 10:31:17 -0600

    creates working service for api call

--- app/controllers/word_search_controller.rb
+++ app/controllers/word_search_controller.rb
@@ -1,5 +1,7 @@
 class WordSearchController < ApplicationController
   def index
-    @word = session[:word]
+    word = session[:word]
+    service = OxfordService.new
+    @examples = service.generate_sentences(word)
   end
 end
--- app/services/oxford_service.rb
+++ app/services/oxford_service.rb
@@ -1,7 +1,7 @@
-class OxfordFacade
+class OxfordService
 
   def generate_sentences(word)
-    get_json("/entries/en/#{word}/sentences")
+    get_json("entries/en/#{word}/sentences")
   end
 
   private
@@ -11,10 +11,16 @@ class OxfordFacade
   end
 
   def conn
-    Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1") do |faraday|
-      req.headers['app_id'] = ENV[:oxford_api_id]
-      req.headers['app_key'] = ENV[:oxford_api_key]
+    Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/") do |faraday|
+      faraday.headers['app_id'] = ENV["oxford_api_id"]
+      faraday.headers['app_key'] = ENV["oxford_api_key"]
       faraday.adapter Faraday.default_adapter
     end
   end
 end
+# https://od-api.oxforddictionaries.com/api/v1/entries/en/mindfulness/sentences
+# Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/mindfulness/sentences") do |faraday|
+#   faraday.headers['app_id'] = ENV["oxford_api_id"]
+#   faraday.headers['app_key'] = ENV["oxford_api_key"]
+#   faraday.adapter Faraday.default_adapter
+# end
